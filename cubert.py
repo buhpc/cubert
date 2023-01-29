b6da97f0bf5a8929edec61464fe0736eae6a349e
r-yin <ryin2@bu.edu>
2023-01-29 18:10:56 -0500

    not useless

--- cubert.py
+++ cubert.py
@@ -6,6 +6,15 @@ color = {
 cube=['w']*9+['o']*9+['g']*9+['r']*9+['b']*9+['y']*9
 
 def print_cube(cube):
-    print(cube)
+    print("{", end='')
+    for i in range(6):
+        print("{", end='')
+        for j in range(9):
+            print(cube[i*9+j], end = '')
+        if i != 5:
+            print("},")
+        else:
+            print("}}")
+    
     
 print_cube(cube)
