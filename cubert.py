d82fb72c06833b7ec7f97568ed216bed2b07680a
carltonknox <cknox@bu.edu>
2023-01-29 17:32:30 -0500

    cubert

---
+++ cubert.py
@@ -0,0 +1,11 @@
+color = {
+"U": "white", "D": "yellow", "L": "orange", "R": "red", "F": "#00aa00", "B": "blue", "X": "darkgray",
+"o": "#fdfdc6", "x": "blueviolet", "y": "magenta"
+}
+
+cube=['w']*9+['o']*9+['g']*9+['r']*9+['b']*9+['y']*9
+
+def print_cube(cube):
+    print(cube)
+    
+print_cube(cube)
