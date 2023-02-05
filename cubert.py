83bca412e395588a0e429426f40d6a40e8582ca4
Carlton Knox <cknox@bu.edu>
2023-02-05 18:12:15 -0500

    changed cubert to class, added seed, run_moves function

--- cubert.py
+++ cubert.py
@@ -1,148 +1,130 @@
-color = {
-"U": "white", "D": "yellow", "L": "orange", "R": "red", "F": "#00aa00", "B": "blue", "X": "darkgray",
-"o": "#fdfdc6", "x": "blueviolet", "y": "magenta"
-}
-
-cube=['w']*9+['o']*9+['g']*9+['r']*9+['b']*9+['y']*9
-
-def print_cube(cube):
-    print("{", end='')
-    for i in range(6):
+class cubert:
+        
+    def print_cube(self):
         print("{", end='')
-        for j in range(9):
-            print(cube[i*9+j], end = '')
-        if i != 5:
-            print("},")
-        else:
-            print("}}")
+        for i in range(6):
+            print("{", end='')
+            for j in range(9):
+                print(self.cube[i*9+j], end = '')
+            if i != 5:
+                print("},")
+            else:
+                print("}}")
     
+    def __perm_cycle(self,a,b,c,d):
+        p_a=self.cube[a[0]*9+a[1]]
+        p_b=self.cube[b[0]*9+b[1]]
+        p_c=self.cube[c[0]*9+c[1]]
+        p_d=self.cube[d[0]*9+d[1]]
+        #copy to next location
+        self.cube[a[0]*9+a[1]]=p_d
+        self.cube[b[0]*9+b[1]]=p_a
+        self.cube[c[0]*9+c[1]]=p_b
+        self.cube[d[0]*9+d[1]]=p_c
+    def w_move(self,direction):
+        f=0 #face
+        a=[4,3,2,1] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],0),(a[1],0),(a[2],0),(a[3],0))
+            self.__perm_cycle((a[0],1),(a[1],1),(a[2],1),(a[3],1))
+            self.__perm_cycle((a[0],2),(a[1],2),(a[2],2),(a[3],2))
+        if(direction==1):
+            self.__perm_cycle((f,6),(f,8),(f,2),(f,0))
+            self.__perm_cycle((f,3),(f,7),(f,5),(f,1))
+            self.__perm_cycle((a[3],0),(a[2],0),(a[1],0),(a[0],0))
+            self.__perm_cycle((a[3],1),(a[2],1),(a[1],1),(a[0],1))
+            self.__perm_cycle((a[3],2),(a[2],2),(a[1],2),(a[0],2))
+
+    def o_move(self,direction):
+        f=1 #face
+        a=[0,2,5,4] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],0),(a[1],0),(a[2],0),(a[3],8))
+            self.__perm_cycle((a[0],3),(a[1],3),(a[2],3),(a[3],5))
+            self.__perm_cycle((a[0],6),(a[1],6),(a[2],6),(a[3],2))
+        if(direction==1):
+            self.__perm_cycle((f,6),(f,8),(f,2),(f,0))
+            self.__perm_cycle((f,3),(f,7),(f,5),(f,1))
+            self.__perm_cycle((a[3],8),(a[2],0),(a[1],0),(a[0],0))
+            self.__perm_cycle((a[3],5),(a[2],3),(a[1],3),(a[0],3))
+            self.__perm_cycle((a[3],2),(a[2],6),(a[1],6),(a[0],6))
+
+    def g_move(self,direction):
+        f=2 #face
+        a=[0,3,5,1] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],7),(a[1],0),(a[2],2),(a[3],8))
+            self.__perm_cycle((a[0],8),(a[1],3),(a[2],1),(a[3],5))
+            self.__perm_cycle((a[0],9),(a[1],6),(a[2],0),(a[3],2))
+        if(direction==1):
+            self.__perm_cycle((f,6),(f,8),(f,2),(f,0))
+            self.__perm_cycle((f,3),(f,7),(f,5),(f,1))
+            self.__perm_cycle((a[3],8),(a[2],2),(a[1],0),(a[0],7))
+            self.__perm_cycle((a[3],5),(a[2],1),(a[1],3),(a[0],8))
+            self.__perm_cycle((a[3],2),(a[2],0),(a[1],6),(a[0],9))
 
 
-def perm_cycle(cube,a,b,c,d):
-    p_a=cube[a[0]*9+a[1]]
-    p_b=cube[b[0]*9+b[1]]
-    p_c=cube[c[0]*9+c[1]]
-    p_d=cube[d[0]*9+d[1]]
-    #copy to next location
-    cube[a[0]*9+a[1]]=p_d
-    cube[b[0]*9+b[1]]=p_a
-    cube[c[0]*9+c[1]]=p_b
-    cube[d[0]*9+d[1]]=p_c
+    def r_move(self,direction):
+        f=3 #face
+        a=[0, 4, 5, 2] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],8),(a[1],0),(a[2],8),(a[3],8))
+            self.__perm_cycle((a[0],5),(a[1],3),(a[2],5),(a[3],5))
+            self.__perm_cycle((a[0],2),(a[1],6),(a[2],2),(a[3],2))
+        elif(direction==1):
+            self.__perm_cycle((f,0),(f,6),(f,8),(f,2))
+            self.__perm_cycle((f,1),(f,3),(f,7),(f,5))
+            self.__perm_cycle((a[3],8),(a[2],8),(a[1],0),(a[0],8))
+            self.__perm_cycle((a[3],5),(a[2],5),(a[1],3),(a[0],5))
+            self.__perm_cycle((a[3],2),(a[2],2),(a[1],6),(a[0],2))
 
+    def b_move(self,direction):
+        f=4 #face
+        a=[0, 1, 5, 3] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],0),(a[1],6),(a[2],8),(a[3],2))
+            self.__perm_cycle((a[0],1),(a[1],3),(a[2],7),(a[3],5))
+            self.__perm_cycle((a[0],2),(a[1],0),(a[2],6),(a[3],8))
+        elif(direction==1):
+            self.__perm_cycle((f,0),(f,6),(f,8),(f,2))
+            self.__perm_cycle((f,1),(f,3),(f,7),(f,5))
+            self.__perm_cycle((a[3],2),(a[2],8),(a[1],6),(a[0],0))
+            self.__perm_cycle((a[3],5),(a[2],7),(a[1],3),(a[0],1))
+            self.__perm_cycle((a[3],8),(a[2],6),(a[1],0),(a[0],2))
 
-def w_move(cube,direction):
-    f=0 #face
-    a=[4,3,2,1] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],0),(a[1],0),(a[2],0),(a[3],0))
-        perm_cycle(cube,(a[0],1),(a[1],1),(a[2],1),(a[3],1))
-        perm_cycle(cube,(a[0],2),(a[1],2),(a[2],2),(a[3],2))
-    if(direction==1):
-        perm_cycle(cube,(f,6),(f,8),(f,2),(f,0))
-        perm_cycle(cube,(f,3),(f,7),(f,5),(f,1))
-        perm_cycle(cube,(a[3],0),(a[2],0),(a[1],0),(a[0],0))
-        perm_cycle(cube,(a[3],1),(a[2],1),(a[1],1),(a[0],1))
-        perm_cycle(cube,(a[3],2),(a[2],2),(a[1],2),(a[0],2))
-
-def o_move(cube,direction):
-    f=1 #face
-    a=[0,2,5,4] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],0),(a[1],0),(a[2],0),(a[3],8))
-        perm_cycle(cube,(a[0],3),(a[1],3),(a[2],3),(a[3],5))
-        perm_cycle(cube,(a[0],6),(a[1],6),(a[2],6),(a[3],2))
-    if(direction==1):
-        perm_cycle(cube,(f,6),(f,8),(f,2),(f,0))
-        perm_cycle(cube,(f,3),(f,7),(f,5),(f,1))
-        perm_cycle(cube,(a[3],8),(a[2],0),(a[1],0),(a[0],0))
-        perm_cycle(cube,(a[3],5),(a[2],3),(a[1],3),(a[0],3))
-        perm_cycle(cube,(a[3],2),(a[2],6),(a[1],6),(a[0],6))
-
-def g_move(cube,direction):
-    f=2 #face
-    a=[0,3,5,1] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],7),(a[1],0),(a[2],2),(a[3],8))
-        perm_cycle(cube,(a[0],8),(a[1],3),(a[2],1),(a[3],5))
-        perm_cycle(cube,(a[0],9),(a[1],6),(a[2],0),(a[3],2))
-    if(direction==1):
-        perm_cycle(cube,(f,6),(f,8),(f,2),(f,0))
-        perm_cycle(cube,(f,3),(f,7),(f,5),(f,1))
-        perm_cycle(cube,(a[3],8),(a[2],2),(a[1],0),(a[0],7))
-        perm_cycle(cube,(a[3],5),(a[2],1),(a[1],3),(a[0],8))
-        perm_cycle(cube,(a[3],2),(a[2],0),(a[1],6),(a[0],9))
-
-
-def r_move(cube,direction):
-    f=3 #face
-    a=[0, 4, 5, 2] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],8),(a[1],0),(a[2],8),(a[3],8))
-        perm_cycle(cube,(a[0],5),(a[1],3),(a[2],5),(a[3],5))
-        perm_cycle(cube,(a[0],2),(a[1],6),(a[2],2),(a[3],2))
-    elif(direction==1):
-        perm_cycle(cube,(f,0),(f,6),(f,8),(f,2))
-        perm_cycle(cube,(f,1),(f,3),(f,7),(f,5))
-        perm_cycle(cube,(a[3],8),(a[2],8),(a[1],0),(a[0],8))
-        perm_cycle(cube,(a[3],5),(a[2],5),(a[1],3),(a[0],5))
-        perm_cycle(cube,(a[3],2),(a[2],2),(a[1],6),(a[0],2))
-
-def b_move(cube,direction):
-    f=4 #face
-    a=[0, 1, 5, 3] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],0),(a[1],6),(a[2],8),(a[3],2))
-        perm_cycle(cube,(a[0],1),(a[1],3),(a[2],7),(a[3],5))
-        perm_cycle(cube,(a[0],2),(a[1],0),(a[2],6),(a[3],8))
-    elif(direction==1):
-        perm_cycle(cube,(f,0),(f,6),(f,8),(f,2))
-        perm_cycle(cube,(f,1),(f,3),(f,7),(f,5))
-        perm_cycle(cube,(a[3],2),(a[2],8),(a[1],6),(a[0],0))
-        perm_cycle(cube,(a[3],5),(a[2],7),(a[1],3),(a[0],1))
-        perm_cycle(cube,(a[3],8),(a[2],6),(a[1],0),(a[0],2))
-
-def y_move(cube,direction):
-    f=5 #face
-    a=[1, 2, 3, 4] #adjacents
-    #direction=0 or 1 (Cw/ccw)
-    if(direction==0):
-        perm_cycle(cube,(f,0),(f,2),(f,8),(f,6))
-        perm_cycle(cube,(f,1),(f,5),(f,7),(f,3))
-        perm_cycle(cube,(a[0],6),(a[1],6),(a[2],6),(a[3],6))
-        perm_cycle(cube,(a[0],7),(a[1],7),(a[2],7),(a[3],7))
-        perm_cycle(cube,(a[0],8),(a[1],8),(a[2],8),(a[3],8))
-    elif(direction==1):
-        perm_cycle(cube,(f,0),(f,6),(f,8),(f,2))
-        perm_cycle(cube,(f,1),(f,3),(f,7),(f,5))
-        perm_cycle(cube,(a[3],6),(a[2],6),(a[1],6),(a[0],6))
-        perm_cycle(cube,(a[3],7),(a[2],7),(a[1],7),(a[0],7))
-        perm_cycle(cube,(a[3],8),(a[2],8),(a[1],8),(a[0],8))
-
-def main():
-    moves=[w_move,o_move,g_move,r_move,b_move,y_move]
-
-    #user input
-    
-    while True:
-        command = input("Enter your command(s): ")
-        if command == "exit":
-            break
-
+    def y_move(self,direction):
+        f=5 #face
+        a=[1, 2, 3, 4] #adjacents
+        #direction=0 or 1 (Cw/ccw)
+        if(direction==0):
+            self.__perm_cycle((f,0),(f,2),(f,8),(f,6))
+            self.__perm_cycle((f,1),(f,5),(f,7),(f,3))
+            self.__perm_cycle((a[0],6),(a[1],6),(a[2],6),(a[3],6))
+            self.__perm_cycle((a[0],7),(a[1],7),(a[2],7),(a[3],7))
+            self.__perm_cycle((a[0],8),(a[1],8),(a[2],8),(a[3],8))
+        elif(direction==1):
+            self.__perm_cycle((f,0),(f,6),(f,8),(f,2))
+            self.__perm_cycle((f,1),(f,3),(f,7),(f,5))
+            self.__perm_cycle((a[3],6),(a[2],6),(a[1],6),(a[0],6))
+            self.__perm_cycle((a[3],7),(a[2],7),(a[1],7),(a[0],7))
+            self.__perm_cycle((a[3],8),(a[2],8),(a[1],8),(a[0],8))
+            
+    def run_moves(self,command):
         i = 0
         while i < len(command):
             current = command[i]
@@ -166,12 +148,10 @@ def main():
             elif current == 'y':
                 move_idx = 5
             
-            moves[move_idx](cube, dir)
-
-        print_cube(cube)
-
-# program exits
+            self.moves[move_idx](dir)
+    
+    def __init__(self,seed):
+        self.cube=['w']*9+['o']*9+['g']*9+['r']*9+['b']*9+['y']*9
+        self.moves=[self.w_move,self.o_move,self.g_move,self.r_move,self.b_move,self.y_move]
         
-            
-if __name__ == "__main__":
-    main()
+        self.run_moves(seed)
---
+++ run_main.py
@@ -0,0 +1,25 @@
+from cubert import cubert
+import sys
+def main():
+    #user input
+    if(len(sys.argv)>1):
+        command=sys.argv[1]
+    else:
+        command=""
+    print("seed: ",command)
+    cube = cubert(command)
+    cube.print_cube()
+    
+    while True:
+        command = input("Enter your command(s): ")
+        if command == "exit":
+            exit()
+        
+        cube.run_moves(command)
+        cube.print_cube()
+
+# program exits
+        
+            
+if __name__ == "__main__":
+    main()
