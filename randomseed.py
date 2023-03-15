20dfd34fae5ad51391ecdabc8e470e78aff2dfb3
cknox <cknox@bu.edu>
2023-03-15 19:59:43 -0400

    randomseed

--- randomseed.py
+++ randomseed.py
@@ -1,14 +1,17 @@
 import sys
 import random
 
-assert(len(sys.argv) == 2)
+def getrandomseed(n_steps):
 
-n_steps = int(sys.argv[1])
+    moves = "wogrby"
+    appendix = ["","'","2"]
 
-moves = "wogrby"
-appendix = ["","'","2"]
+    randomseed = ""
+    for i in range(n_steps):
+        randomseed+=random.choice(moves)+random.choice(appendix)
+    return randomseed
 
-randomseed = ""
-for i in range(n_steps):
-    randomseed+=random.choice(moves)+random.choice(appendix)
-print(randomseed)
+if __name__ == "__main__":
+    assert(len(sys.argv) == 2)
+    n_steps = int(sys.argv[1])
+    print(getrandomseed(n_steps))
